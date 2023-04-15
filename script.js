2b16089cd771f5cd3be5349e5ad84c605cd89096
Barcelos <barcelosps41710@gmail.com>
2023-04-15 20:20:23 -0300

    fazendo

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const colorPalette = document.querySelector('#color-palette');
-const colors = colorPalette.querySelectorAll('.randomColor');
-
-function generateRandomColor() {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return "#" + randomColor;
-}
-
-document.querySelector('#button-random-color').addEventListener('click', () => {
-  colors.forEach(color => {
-    color.style.backgroundColor = generateRandomColor();
-    });
-});   
-
 });
-
