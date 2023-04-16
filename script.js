b5c3f66ad6a593ad95242a1eb2c54a245d6eca5c
Barcelos <barcelosps41710@gmail.com>
2023-04-15 22:07:56 -0300

    fiz ate o exercicio 8

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const colorPalette = document.documentElement.querySelector('#color-palette');
+    const color = colorPalette.querySelectorAll('.randomColor');
+    let savedColor = JSON.parse(localStorage.getItem('colorPalette')) || {};
+  
+    function gerarCor (){
+      const Gerar = Math.floor(Math.random() * 16777215).toString(16);
+      return '#' + Gerar;
+    }
+  
+    color[0].style.backgroundColor = 'black';
+  
+    if (Object.keys(savedColor).length !== 0) {
+      for (let i = 1; i < color.length; i++) {
+        color[i].style.backgroundColor = savedColor[i] || gerarCor();
+      }
+    } else {
+      for (let i = 1; i < color.length; i++) {
+        color[i].style.backgroundColor = gerarCor();
+        savedColor[i] = color[i].style.backgroundColor;
+      }
+      localStorage.setItem('colorPalette', JSON.stringify(savedColor));
+    }
+  
+    document.querySelector('#button-random-color').addEventListener('click', () => {
+      for (let i = 1; i < color.length; i++) {
+        color[i].style.backgroundColor = gerarCor();
+        savedColor[i] = color[i].style.backgroundColor;
+      }
+      localStorage.setItem('colorPalette', JSON.stringify(savedColor));
+    });
 
-});
+    const black = document.querySelector('#color0');
+    
+    black.classList.add('selected');
+
+    const colors = document.querySelectorAll('.color');
+    colors.forEach(color => {
+        if (color !== black) {
+        color.classList.remove('selected');
+        }
+    });
+
+  });
